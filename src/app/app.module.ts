34bf360f7a55a0f82c70338f838baff62c7fb169
Jesse de Zwart <jessedezwart@live.nl>
2022-12-11 15:44:29 +0100

    Feat : load questionnaire overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { UsersComponent } from './users/users.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbDropdownModule
+    NgbDropdownModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/questionnaires/questionnaires.component.ts
+++ src/app/questionnaires/questionnaires.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+interface Questionnaire {
+  id: number;
+  name: string;
+  date: string;
+  entryCount: number;
+  author: string;
+}
 
 @Component({
   selector: 'app-questionnaires',
   templateUrl: './questionnaires.component.html',
-  styleUrls: ['./questionnaires.component.scss']
+  styleUrls: ['./questionnaires.component.scss'],
 })
 export class QuestionnairesComponent {
+  questionnaires: Questionnaire[] = [];
+  constructor(private http: HttpClient) {
+    const headers = new HttpHeaders()
+    .set('content-type', 'application/json')
+    .set(
+      'Authorization',
+      'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVc2VyIERldGFpbHMiLCJpc3MiOiJJLlF1ZXN0aW9uIiwiaWF0IjoxNjcwNzY4MjczLCJlbWFpbCI6InNwaW5lX2FkbWluQHRlc3QuY29tIn0.j4SBytNDMTXXyHimJFaFWwuM1VBTsqvnCBVV7cD4zuo'
+    );
+  this.http
+    .get('http://localhost:8080/questionnaire/all', { headers: headers })
+    .subscribe((data) => {
+      this.populateQuestionnaires(data);
+    });
+  }
 
+  populateQuestionnaires(data: any) {
+    for (let i = 0; i < data.length; i++) {
+      this.questionnaires.push({
+        id: data[i].id,
+        name: data[i].name,
+        date: '',
+        entryCount: 0,
+        author: '',
+      });
+    }
+  }
 }

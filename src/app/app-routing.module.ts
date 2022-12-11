b2697c53dd812c463dee645ee7a966a4e992c821
Jesse de Zwart <jessedezwart@live.nl>
2022-12-11 14:23:40 +0100

    Feat: add basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./login/login.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { QuestionnairesComponent } from './questionnaires/questionnaires.component';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
+  {path: 'questionnaires', component: QuestionnairesComponent},
+  {path: 'users', component: UsersComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { QuestionnairesComponent } from './questionnaires/questionnaires.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavigationComponent,
+    QuestionnairesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]

5259ed48322a153ae6177f075a767f708552c797
Luuk <Luuk3706@Gmail.com>
2022-12-13 17:22:02 +0100

    Chore: Cleaned up components and refactored unclear variable names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
+import {createComponent, NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { QuestionnairesComponent } from './questionnaires/questionnaires.component';
 import { UserComponent } from './user/user.component';
+import {CreateComponent} from "./user/create/create.component";
+import {OverviewComponent} from "./user/overview/overview.component";
+import {EditComponent} from "./user/edit/edit.component";
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'questionnaires', component: QuestionnairesComponent},
-  {path: 'user', component: UserComponent}
+  {path: 'user', component: UserComponent},
+  {path: 'userOverview', component: OverviewComponent},
+  {path: 'userCreate', component: CreateComponent},
+  {path: 'userEdit', component: EditComponent},
 ];
 
 @NgModule({
--- src/app/user/create/create.component.ts
+++ src/app/user/create/create.component.ts
@@ -10,10 +10,10 @@ import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 export class CreateComponent {
   registerForm = new FormGroup({
-    inputEmail: new FormControl(null, [Validators.required, Validators.email, Validators.pattern(/^(?=.{1,64}@)[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})$/)]),
-    inputName: new FormControl(null, Validators.required),
-    inputOrganization: new FormControl(null, Validators.required),
-    inputRole: new FormControl(null, Validators.required)
+    registerEmail: new FormControl(null, [Validators.required, Validators.email, Validators.pattern(/^(?=.{1,64}@)[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})$/)]),
+    registerName: new FormControl(null, Validators.required),
+    registerOrganization: new FormControl(null, Validators.required),
+    registerRole: new FormControl(null, Validators.required)
   })
 
 
@@ -24,9 +24,9 @@ export class CreateComponent {
     const headers = new HttpHeaders().set('content-type', 'application/json')
       .set('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVc2VyIERldGFpbHMiLCJpc3MiOiJJLlF1ZXN0aW9uIiwiaWF0IjoxNjcwNzY4MjczLCJlbWFpbCI6InNwaW5lX2FkbWluQHRlc3QuY29tIn0.j4SBytNDMTXXyHimJFaFWwuM1VBTsqvnCBVV7cD4zuo');
     this.http.post('http://localhost:8080/auth/register/', {
-      name: this.registerForm.value.inputName,
-      email: this.registerForm.value.inputEmail,
-      organization: this.registerForm.value.inputOrganization,
+      name: this.registerForm.value.registerName,
+      email: this.registerForm.value.registerEmail,
+      organization: this.registerForm.value.registerOrganization,
       role: 'SPINE_ADMIN',
     }, {headers: headers})
       .subscribe();

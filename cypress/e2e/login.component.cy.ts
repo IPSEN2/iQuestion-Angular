1c2cda341dc7c9628be2fe9522a909cbd93595dd
VincevdZwet <vincevanderzwet@live.nl>
2022-12-15 12:45:25 +0100

    Test: install cypress for e2e tests & add first login e2e test

---
+++ cypress/e2e/login.component.cy.ts
@@ -0,0 +1,29 @@
+describe('test', () => {
+  it('should not login if email adress is invalid', function () {
+    cy.visit("/");
+    cy.url().should('includes', '');
+    cy.get('[formControlName="email"').type('geenAtSign');
+    cy.get('[formControlName="password"').type('TestLangGenoegWachtwoord1!');
+    cy.get('button').should('be.disabled');
+  });
+
+  it('Should not login if password is to short', () => {
+    cy.visit("/");
+    cy.url().should('includes', '');
+    cy.get('[formControlName="email"').type('geenAtSign');
+    cy.get('[formControlName="password"').type('Kort@!');
+    cy.get('button').should('be.disabled');
+  });
+
+  it('should not login if invalid but validated credentials are provided', function () {
+    // cy.intercept("POST", 'http://localhost:8080/auth/login', {fixture: 'loginFailed.json'});
+
+    cy.visit("/");
+    cy.url().should('includes', '');
+    cy.get('[formControlName="email"').type('test@account.com');
+    cy.get('[formControlName="password"').type('TestLangGenoeg1!');
+    cy.get('button').click();
+    cy.url().should('includes', '');
+    cy
+  });
+})

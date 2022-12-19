a8fe553b6f1f2ec5030b3da3612e8221c5bfd2d4
Stan Smits <s1136644@student.hsleiden.nl>
2022-12-19 14:11:38 +0100

    feat: change statusmessages to toasts

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,26 +1,20 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, setTestabilityGetter } from '@angular/core';
 import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastService } from '../shared/toast/toast-service';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss'],
 })
-
 export class ChangePasswordComponent implements OnInit {
   changePasswordForm!: FormGroup;
   resetTokenForm!: FormGroup;
 
-  showAlert: boolean = false;
-  alertType: String = 'danger';
-  statusMessage!: String;
-
-  isLoading: boolean = false;
-
   showTokenResetBody: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private toastService: ToastService) {}
 
   ngOnInit() {
     this.initForm();
@@ -29,7 +23,7 @@ export class ChangePasswordComponent implements OnInit {
   private initForm(): void {
     this.changePasswordForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
-    });    
+    });
     this.resetTokenForm = new FormGroup({
       resetToken: new FormControl(null, [Validators.required]),
       newPassword: new FormControl(null, [Validators.required]),
@@ -39,38 +33,48 @@ export class ChangePasswordComponent implements OnInit {
   onChangePassword() {
     const url = '/auth/request-password-reset';
     const email = this.changePasswordForm.value['email'];
-    this.isLoading = true;
+    this.toastService.show(
+      '⚙️ - Bezig met het aanvragen van een nieuw wachtwoord...',
+      { classname: 'bg-info text-light', delay: 3000 }
+    );
 
     this.http.post(url, { email: email }).subscribe(
       (response) => {
-        this.showAlert = true;
-        this.alertType = 'success';
-        this.statusMessage = 'Er is een email verstuurd met een reset link';
+        this.toastService.show(
+          '✅ - We hebben een email gestuurd met je verficatie token!',
+          { classname: 'bg-success text-light', delay: 5000 }
+        );
         this.showTokenResetBody = true;
-
       },
       (error) => {
-        this.showAlert = true;
-        this.alertType = 'danger';
-        this.isLoading = true;
         let statuscode = error.error.status;
         switch (statuscode) {
           case 400:
-            this.statusMessage = 'Je heb al een reset link ontvangen, bekijk je email';
+            this.toastService.show(
+              '❌ - Dit account heeft al een token ontvangen, vul hem op de volgende pagina in',
+              { classname: 'bg-danger text-light', delay: 5000 }
+            );
             setTimeout(() => {
               this.showTokenResetBody = true;
-              this.showAlert = false;
             }, 2500);
             break;
           case 404:
-            this.statusMessage = 'Er is geen account met dit email adres';
+            this.toastService.show(
+              '❌ - Dit email adres is niet bekend bij ons, probeer het opnieuw',
+              { classname: 'bg-danger text-light', delay: 5000 }
+            );
             break;
           case 500:
-            this.statusMessage = 'Er is iets mis gegaan, probeer het later opnieuw'
-            ;
+            this.toastService.show(
+              '❌ - Er is een onbekende fout opgetreden, probeer het later opnieuw',
+              { classname: 'bg-danger text-light', delay: 5000 }
+            );
             break;
           default:
-            this.statusMessage = 'Er is een onbekende fout opgetreden, probeer het later opnieuw';
+            this.toastService.show(
+              '❌ - Er is een onbekende fout opgetreden, probeer het later opnieuw',
+              { classname: 'bg-danger text-light', delay: 5000 }
+            );
             break;
         }
       }
@@ -80,30 +84,36 @@ export class ChangePasswordComponent implements OnInit {
   onResetPassword() {
     let token = this.resetTokenForm.value['resetToken'];
     let newPassword = this.resetTokenForm.value['newPassword'];
-
+    this.toastService.show('⚙️ - Bezig met het veranderen van je wachtwoord...',
+    { classname: 'bg-info text-light', delay: 3000 }
+  );
 
     const url = '/auth/change-password';
-    this.isLoading = true;
 
-    this.http.post(url, { token: token, newPassword: newPassword}).subscribe(
+    this.http.post(url, { token: token, newPassword: newPassword }).subscribe(
       (response) => {
-        this.showAlert = true;
-        this.alertType = 'success';
-        this.statusMessage = 'Je wachtwoord is succesvol gewijzigd';
+        this.toastService.show(
+          '✅ - Je wachtwoord is succesvol gewijzigd, je wordt doorgestuurd naar de login pagina',
+          { classname: 'bg-success text-light', delay: 3000 }
+        );
         setTimeout(() => {
           window.location.href = '/login';
         }, 2500);
       },
       (error) => {
         let statuscode = error.error.status;
-        this.showAlert = true;
-        this.alertType = 'danger';
         switch (statuscode) {
           case 404:
-            this.statusMessage = 'Deze reset token is niet meer geldig of bestaat niet, heb je je wachtwoord al gewijzigd?';
+            this.toastService.show(
+              '❌ - Dit token is niet bekend bij ons, probeer het opnieuw',
+              { classname: 'bg-danger text-light', delay: 5000 }
+            );
             break;
           default:
-            this.statusMessage = 'Er is een onbekende fout opgetreden, probeer het later opnieuw';
+            this.toastService.show(
+              '❌ - Er is een onbekende fout opgetreden, probeer het later opnieuw',
+              { classname: 'bg-danger text-light', delay: 5000 }
+            );
             break;
         }
       }

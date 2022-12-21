157e39f5a9b87e3ceb3fcb94afba41be006839a2
VincevdZwet <vincevanderzwet@live.nl>
2022-12-21 23:52:23 +0100

    Test: add E2E tests for the loginComponent and the createUsercomponent

--- cypress/e2e/login.component.cy.ts
+++ cypress/e2e/login.component.cy.ts
@@ -32,17 +32,16 @@ describe('LoginComponent', () => {
     cy.url().should('not.include', 'questionnaires');
   });
 
-  // it('Should login when valid and validated credentials are provided', () => {
-  //
-  //   cy.intercept("POST", 'http://localhost:8080/auth/login',{fixture: 'loginSuccess.json'})
-  //
-  //   cy.visit('/');
-  //   cy.url().should('includes', '');
-  //   cy.get('[formControlName="email"]').type('s1133277@student.hsleiden.nl');
-  //   cy.get('[formControlName="password"]').type('12345678');
-  //   cy.get('button').click();
-  //
-  //   cy.url().should('include', 'questionnaires');
-  //   cy.get('#homeHeader').should('contain', 'Welkom bij deze Cypress Test App');
-  // });
+  it('Should login when valid and validated credentials are provided', () => {
+
+    cy.intercept("POST", 'http://localhost:8080/auth/login',{fixture: 'loginSuccess.json'})
+
+    cy.visit('/');
+    cy.url().should('includes', '');
+    cy.get('[formControlName="email"]').type('s1133277@student.hsleiden.nl');
+    cy.get('[formControlName="password"]').type('12345678');
+    cy.get('button').click();
+
+    cy.url().should('include', 'questionnaires');
+  });
 })

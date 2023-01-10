4992e1e4fe31c791d71388a701abf4f67b69109c
Stan Smits <s1136644@student.hsleiden.nl>
2023-01-10 15:55:51 +0100

    fix: spine admin can now view same as user, table outlining shows on caregiver view, localuser service usage for obtaining data

--- src/app/questionnaires/questionnaires.component.ts
+++ src/app/questionnaires/questionnaires.component.ts
@@ -2,19 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { QuestionnaireService } from '../service/api/questionnaire.service';
 import { Questionnaire } from '../shared/models/questionnaire.model';
 import { User } from '../shared/models/user.model';
+import { LocalUserService } from '../shared/services/localUser.service';
 
 @Component({
   selector: 'app-questionnaires',
   templateUrl: './questionnaires.component.html',
   styleUrls: ['./questionnaires.component.scss'],
 })
-export class QuestionnairesComponent implements OnInit {
-  questionnaires: Questionnaire[] = [];
-  user = JSON.parse(localStorage.getItem("userData") || sessionStorage.getItem("userData") || "{}")
-
-  constructor(private questionnaireService: QuestionnaireService) {
+export class QuestionnairesComponent {
+  constructor(private questionnaireService: QuestionnaireService, private localUserService: LocalUserService) {
     this.questionnaireService
       .getAll()
       .subscribe((questionnaires) => (this.questionnaires = questionnaires));
   }
+
+  questionnaires: Questionnaire[] = [];
+  user = this.localUserService.localUser
 }

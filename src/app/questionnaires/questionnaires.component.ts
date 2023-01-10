6247a7443aea201f61239a72ca18f3b02e6f8645
Stan Smits <s1136644@student.hsleiden.nl>
2023-01-10 11:21:26 +0100

    FEAT: Based on user role the buttons change

--- src/app/questionnaires/questionnaires.component.ts
+++ src/app/questionnaires/questionnaires.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { QuestionnaireService } from '../service/api/questionnaire.service';
 import { Questionnaire } from '../shared/models/questionnaire.model';
+import { User } from '../shared/models/user.model';
 
 @Component({
   selector: 'app-questionnaires',
   templateUrl: './questionnaires.component.html',
   styleUrls: ['./questionnaires.component.scss'],
 })
-export class QuestionnairesComponent {
+export class QuestionnairesComponent implements OnInit {
   questionnaires: Questionnaire[] = [];
+  user = JSON.parse(sessionStorage.getItem("userData") || "{}")
 
   constructor(private questionnaireService: QuestionnaireService) {
     this.questionnaireService
       .getAll()
       .subscribe((questionnaires) => (this.questionnaires = questionnaires));
+  }
+
+  ngOnInit(): void {
+      console.log(this.user.user)
   }
 }

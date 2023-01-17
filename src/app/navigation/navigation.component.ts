7ad1dfc86c5c066c9f44a5a21fc149ae8625dbe8
Stan <smitsstan2004@gmail.com>
2023-01-17 14:46:21 +0100

    Fix: minor changes that account to alot of total changes

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import {AuthService} from "../auth/auth.service";
 import {LocalUserService} from "../shared/services/localUser.service";
 
@@ -11,11 +12,23 @@ export class NavigationComponent implements OnInit {
   navOpen: boolean = false;
   user: any;
 
-  constructor(private authService: AuthService, private localUserService: LocalUserService) {
+  constructor(private authService: AuthService, private localUserService: LocalUserService, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.user = this.localUserService.localUser;
+  }
+
+  getRoute() {
+    // Returns text of the current route
+    // Example: /users => Users
+    // If path is empty, return questionnaires
+    let path = this.activatedRoute.snapshot.routeConfig?.path;
+    console.log(path, this.activatedRoute.snapshot.routeConfig?.path);
+    if (path === '') {
+      return 'questionnaires';
+    }
+    return path;
   }
 
   onLogout() {

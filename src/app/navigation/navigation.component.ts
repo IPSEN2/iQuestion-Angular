3f8b8e7e303442111bfe9680f3eccc979e691bff
Stan <smitsstan2004@gmail.com>
2023-01-18 16:49:54 +0100

    fix: navbar mobile view and login view change

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import {AuthService} from "../auth/auth.service";
 import {LocalUserService} from "../shared/services/localUser.service";
@@ -11,23 +12,19 @@ import {LocalUserService} from "../shared/services/localUser.service";
 export class NavigationComponent implements OnInit {
   navOpen: boolean = false;
   user: any;
+  collapsed: boolean = true;
+  // Get the full route, including the parent route
+  
+  path:string = this.activatedRoute.snapshot.parent?.parent?.routeConfig?.path || this.activatedRoute.snapshot.parent?.routeConfig?.path || this.activatedRoute.snapshot.routeConfig?.path || '';
+
 
   constructor(private authService: AuthService, private localUserService: LocalUserService, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.user = this.localUserService.localUser;
-  }
 
-  getRoute() {
-    // Returns text of the current route
-    // Example: /users => Users
-    // If path is empty, return questionnaires
-    let path = this.activatedRoute.snapshot.routeConfig?.path;
-    if (path === '') {
-      return 'questionnaires';
-    }
-    return path;
+    console.log(this.path)
   }
 
   onLogout() {

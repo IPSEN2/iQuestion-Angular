51e7c6b47b00f5c0fd7c7822bb10745f65a3c1bc
VincevdZwet <vincevanderzwet@live.nl>
2023-01-19 16:01:52 +0100

    Refactor: Make navbar responsive

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import { provideProtractorTestingSupport } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
 import {AuthService} from "../auth/auth.service";
 import {LocalUserService} from "../shared/services/localUser.service";
 
@@ -10,21 +8,14 @@ import {LocalUserService} from "../shared/services/localUser.service";
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-  navOpen: boolean = false;
   user: any;
   collapsed: boolean = true;
-  // Get the full route, including the parent route
-  
-  path:string = this.activatedRoute.snapshot.parent?.parent?.routeConfig?.path || this.activatedRoute.snapshot.parent?.routeConfig?.path || this.activatedRoute.snapshot.routeConfig?.path || '';
 
-
-  constructor(private authService: AuthService, private localUserService: LocalUserService, private activatedRoute: ActivatedRoute) {
+  constructor(private authService: AuthService, private localUserService: LocalUserService) {
   }
 
   ngOnInit() {
     this.user = this.localUserService.localUser;
-
-    console.log(this.path)
   }
 
   onLogout() {

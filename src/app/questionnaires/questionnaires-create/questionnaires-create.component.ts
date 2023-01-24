7c4f95d2ffa4889b8c7450323f854fefe06290ea
Stan Smits <s1136644@student.hsleiden.nl>
2023-01-25 00:12:13 +0100

    fix removed duped request

--- src/app/questionnaires/questionnaires-create/questionnaires-create.component.ts
+++ src/app/questionnaires/questionnaires-create/questionnaires-create.component.ts
@@ -123,24 +123,7 @@ export class QuestionnairesCreateComponent {
         question.options = question.options.split('\n');
       });
     });
-
-    this.http.put('/questionnaire/', this.questionnaireForm.value).subscribe({
-      next: () => {
-        this.toastService.show('Vragenlijst is aangemaakt!', {
-          classname: 'bg-success text-light',
-          delay: 3000,
-        });
-        this.questionnaireForm.reset();
-      },
-      error: (error) => {
-        this.toastService.show('Er is iets mis gegaan', {
-          classname: 'bg-danger text-light',
-          delay: 3000,
-        });
-      },
-    });
-
-    // Post to API
+    
     this.http.put('/questionnaire/', this.questionnaireForm.value).subscribe({
       next: () => {
         this.toastService.show('✅ - Vragenlijst is aangemaakt!', {

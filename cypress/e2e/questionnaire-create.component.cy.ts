b59f23dfc7d090268313696fff4b974ecd17ed6f
SafinB <s1135903@student.hsleiden.nl>
2023-01-25 00:22:17 +0100

    Feat: Added creating questionnaire test

--- cypress/e2e/questionnaire-create.component.cy.ts
+++ cypress/e2e/questionnaire-create.component.cy.ts
@@ -86,4 +86,37 @@ describe('QuestionnairesCreateComponent', () => {
     //Should delete a question in the questionnaire form
     cy.get('#vraagVerwijderen').click({force: true});
   })
+
+  it('Should fill in all the forms and make a questionnaire', () => {
+    cy.visit('/');
+    cy.url().should('includes', '');
+    cy.get('[formControlName="email"]').type('spineadmin@test.com');
+    cy.get('[formControlName="password"]').type('12345678');
+    cy.get('button').click();
+    cy.url().should('include', 'questionnaires');
+
+    // should go to create questionnaire page
+    cy.visit('/questionnaires/new');
+    cy.url().should('include', 'questionnaires/new');
+
+    cy.get('#name').type('test');
+    cy.get('#description').type('test');
+
+    // should add a segment in order to add a question
+    cy.get('#segmentToevoegen').click();
+
+    // fill in segment form
+    cy.get('#naamSegment').type('test');
+    cy.get('#beschrijving').type('test');
+
+    // should add a question in order to fill in the form
+    cy.get('#vraagToevoegen').click();
+
+    // fill in question form
+    cy.get('#vraagNaam').type('test', {force: true});
+    cy.get('#vraagOpties').type('Optie a \n Optie b \n Optie c \n Optie d', {force: true});
+
+    // should make a questionnaire
+    cy.get('#submit').click({force: true});
+  })
 });
